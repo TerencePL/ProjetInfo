e57892635523a9c20742619d9e427bf22bce5e9b
TerencePL <36531921+TerencePL@users.noreply.github.com>
2020-04-16 17:12:41 +0200

    Page acheteur/vendeur front end +

--- Page/Acheteur.php
+++ Page/Acheteur.php
@@ -32,9 +32,7 @@ session_start();
       <?php //**************************[[A REMPLACER PAR DU TEXTE SI PERSONNE N'EST CONNECTE]]****************************** ?>
 
  
-          <h1>Bienvenue: </h1>
-
-            <?php // recupération des infos du compte connecté depuis la BDD via son ID
+          <h1><font size="10pt"><B>Bienvenue à toi petit acheteur<?php // recupération des infos du compte connecté depuis la BDD via son ID
               $database = "ebayece";
 
               $db_handle = mysqli_connect('localhost', 'root', '');
@@ -46,18 +44,20 @@ session_start();
 
               $result = mysqli_query($db_handle,"SELECT * FROM acheteur WHERE ID LIKE '$ID'");
               $row = mysqli_fetch_array($result);
-          		}
+              }
             ?>
 
 
               <ul>
-                <li>  <?php if(isset($_SESSION['ID'])) {echo "Nom: ".$row['Nom'];} ?> </li>
-                <li> <?php if(isset($_SESSION['ID'])) {echo "Prenom: ".$row['Prenom'];}?> </li> 
+                <li style="color:white"> <font size="+4"> <?php if(isset($_SESSION['ID'])) {echo "".$row['Prenom'];}?><?php if(isset($_SESSION['ID'])) {echo " ".$row['Nom'];} ?></font> </li> 
+             
                     
               </ul>  
 
               <br>
-              <a href="http://localhost/ProjetInfo/Deconnexion/Deconnexion.php" title="Deconnexion">Deconnexion</a>
+              <a href="http://localhost/ProjetInfo/Deconnexion/Deconnexion.php" title="Deconnexion" style="color:red"><font size="+4">Déconnexion </font></a></B></font></h1>
+
+            
 
 
         <?php //********************************************************************************************************* ?>
@@ -65,37 +65,7 @@ session_start();
         </div>
   </header>
 
-  <div class="container features">
-   <div class="row">
-   <div class="col-lg-4 col-md-4 col-sm-12">
-   <h3 class="feature-title">Tout savoir sur les Cariatides</h3>
-   <img src="cariatides.jpg" class="img-fluid">
-   <p>
-    Une cariatide, ou caryatide (du grec ancien Καρυάτιδες, littéralement « femmes de Caryes », du nom d'une ville de Laconie), est une statue de femme souvent vêtue d'une longue tunique, soutenant un entablement sur sa tête ; remplaçant ainsi une colonne, un pilier ou un pilastre, les Caryatides apparaissent essentiellement sur les édifices d'ordre ionique. 
-   </p>
-   </div>
-   <div class="col-lg-4 col-md-4 col-sm-12">
-   <h3 class="feature-title">Forêt urbaine ou la culpabilisation existentielle de l’Autre</h3>
-   <img src="column2.jpg" class="img-fluid">
-   <p>
-    N’y a-t-il pas un abus de langage à utiliser cet oxymore qu’est la forêt urbaine ? L’expression esquisse au crayon magique les contours de dérive sémantique et de manipulation idéologique certifiées. La forêt urbaine, comme la promesse de son idéal, nous ment.
-   </p>
-   </div>
-   <div class="col-lg-4 col-md-4 col-sm-12">
-    <h3 class="feature-title">Entrer en contact!</h3>
-  <div class="form-group">
-   <input type="text" class="form-control" placeholder="Votre nom:" name="">
-  </div>
-  <div class="form-group">
-   <input type="email" class="form-control" placeholder="Courriel:" name="email">
-  </div>
-  <div class="form-group">
-   <textarea class="form-control" rows="4">Vos commentaires</textarea>
-  </div>
-  <input type="submit" class="btn btn-secondary btn-block" value="Envoyer" name="">
-   </div>
-  </div>
-  </div>
+
 
   <?php include("C:\wamp64\www\ProjetInfo\BasDePage.php"); ?>
 
